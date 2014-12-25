f205ba99ba47e3ee351d8a138c95108819d48a35
rcouchh <rcouch358@comcast.net>
2014-12-25 01:39:47 -0500

    first commit

---
+++ src/board.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class board {
+	
+	final static int bSize=12;
+	
+	int[][] board = new int[bSize][bSize];
+	
+	
+	
+	private void createGUI(){
+		DrawingPanel panel = new DrawingPanel();
+	}
+	
+	public static void drawHex(int i, int j, Graphics2D g2){
+		
+	}
+	
+}
---
+++ src/player.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+
+public class player {
+	String name;
+	int order;
+	int buildingCount;
+	Color color;
+	//Hexagon[] hexsOwned;
+	
+	public player(String name){
+		this.name = name;
+	//	this.color = color;
+	}
+	
+}
